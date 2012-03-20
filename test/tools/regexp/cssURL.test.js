0732b1acf44194877e87085bb4ad32b9a9431a8d
Johannes <mail@johannesewald.de>
2012-03-20 15:30:38 +0100

    started to bundle css with pages and views

---
+++ test/tools/regexp/cssURL.test.js
@@ -0,0 +1,8 @@
+var cssURL = require("../../../lib/tools/regexp/cssURL"),
+    expect = require("expect.js");
+
+describe("cssURL", function () {
+    it("should match url(\"http://www.example.com/\")", function () {
+        throw new Error("Finish test"); //hehehe
+    });
+});
