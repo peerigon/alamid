1d05b353baeb46b57b7ff84531437f5a8ac0b2bd
meaku <michael.jaser@roomieplanet.de>
2012-05-31 15:43:38 +0200

    - added readConfig and non-finished test

---
+++ test/core.readConfig.test.js
@@ -0,0 +1,8 @@
+var expect = require("expect.js"),
+    readConfig = require("../lib/core/readConfig.js");
+
+describe("readConfig", function() {
+
+    it("should read the default config if nothing was passed")
+
+});
