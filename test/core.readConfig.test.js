0cc92ae0faf74d7984e115a5d9522d0ef0631b53
Johannes <mail@johannesewald.de>
2012-05-31 15:56:08 +0200

    readConfig test

--- test/core.readConfig.test.js
+++ test/core.readConfig.test.js
@@ -1,8 +1,13 @@
 var expect = require("expect.js"),
-    readConfig = require("../lib/core/readConfig.js");
+    readConfig = require("../lib/core/readConfig.js"),
+    defaultConfig = require("../lib/core/config.json");
 
 describe("readConfig", function() {
+    var result;
 
-    it("should read the default config if nothing was passed")
+    it("should read the default config if nothing was passed", function () {
+        result = readConfig();
+        expect(result).to.eql(defaultConfig);
+    });
 
 });
