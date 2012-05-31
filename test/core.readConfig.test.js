357d6955d9e4a019ec10e2fc75103f3764a64b65
meaku <michael.jaser@roomieplanet.de>
2012-05-31 17:14:58 +0200

    - config-handling implemented - completed tests for config-handling

--- test/core.readConfig.test.js
+++ test/core.readConfig.test.js
@@ -21,10 +21,10 @@ describe("readConfig", function() {
 
     var result;
 
-     it("should read the default config if nothing was passed", function () {
+    it("should read the default config if nothing was passed", function () {
         result = readConfig();
         expect(result).to.eql(defaultConfig);
-     });
+    });
 
 
     it("should read a custom config if passed via args", function (done) {
@@ -54,6 +54,27 @@ describe("readConfig", function() {
 
         checkConfigViaSubprocess("--port 9099", {} , function(parsedConf) {
             expect(parsedConf.port).to.equal(9099);
+            done();
+        });
+    });
+
+    it("should respect the hierachy and prefer command given via argv", function (done) {
+
+        var relativePathToTestConf = path.relative(process.cwd(), __dirname + "/core.readConfig/customConfig.json");
+
+        checkConfigViaSubprocess("--port 9099", { "config" : relativePathToTestConf }, function(parsedConf) {
+            expect(parsedConf.port).to.equal(9099);
+            done();
+        });
+    });
+
+    it("should respect the hierachy and prefer command given via env before argv", function (done) {
+
+        var relativePathToTestConf = path.relative(process.cwd(), __dirname + "/core.readConfig/customConfig.json");
+        var relativePathToTestConf2 = path.relative(process.cwd(), __dirname + "/core.readConfig/customConfig2.json");
+
+        checkConfigViaSubprocess("--config " + relativePathToTestConf2, { "config" : relativePathToTestConf }, function(parsedConf) {
+            expect(parsedConf.port).to.equal(5555);
             done();
         });
     });
