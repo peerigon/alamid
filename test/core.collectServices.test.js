73b79577dbcd06d3272f256b88eff8982a421d1b
Johannes <mail@johannesewald.de>
2012-06-04 16:46:06 +0200

    - implemented collectServices - tests are running

--- test/core.collectServices.test.js
+++ test/core.collectServices.test.js
@@ -1,1 +1,51 @@
 "use strict"; // run code in ES5 strict mode
+
+var expect = require("expect.js"),
+    rewire = require("rewire"),
+    collectServices,
+    testFolder = __dirname + "/core.collectServices",
+    servicesFolder = testFolder + "/compiled/services";
+
+describe("collectServices", function () {
+    beforeEach(function () {
+        rewire.reset();
+    });
+    it("should collect appropriately", function (done) {
+        var expectedServices = {
+                server: {},
+                client: {}
+            };
+
+        function onCollectServicesEnd(err, services) {
+            expect(err).to.be(null);
+            expect(services).to.eql(expectedServices);
+            expect(Object.keys(services.server)).to.have.length(3);
+            expect(Object.keys(services.client)).to.have.length(3);
+            done();
+        }
+
+        expectedServices.server[servicesFolder + "/A/ServiceA.class.server.js"] = true;
+        expectedServices.server[servicesFolder + "/B/ServiceB.class.server.js"] = true;
+        expectedServices.server[servicesFolder + "/ServiceC.class.server.js"] = true;
+        expectedServices.client[servicesFolder + "/A/ServiceA.class.client.js"] = true;
+        expectedServices.client[servicesFolder + "/B/ServiceB.class.client.js"] = true;
+        expectedServices.client[servicesFolder + "/ServiceC.class.client.js"] = true;
+
+        collectServices = rewire("../lib/core/collectServices");
+        collectServices(testFolder, onCollectServicesEnd);
+    });
+    it("should abort on error", function (done) {
+        /*
+        function onCollectServicesError(err) {
+            expect(err instanceof Error).to.be(true);
+            done();
+        }
+
+        collectServices = rewire("../lib/core/collectServices", null, null, ["finder"]);
+        collectServices(testFolder, onCollectServicesError);
+        console.log(collectServices);
+        //collectServices.__.finder.emit("error", new Error());
+        */
+        //TODO: Include test when rewire()-leaks work properly
+    });
+});
--- test/test.js
+++
@@ -1,5 +0,0 @@
-"use strict"; // run code in ES5 strict mode
-
-var test = require("../");
-
-test.start();
