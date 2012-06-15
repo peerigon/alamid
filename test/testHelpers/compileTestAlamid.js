773c38699e5216fe99336c4473f8febbd44c2b3c
meaku <michael.jaser@roomieplanet.de>
2012-06-15 12:01:44 +0200

    - removed all logs in testing mode!

--- test/testHelpers/compileTestAlamid.js
+++ test/testHelpers/compileTestAlamid.js
@@ -1,8 +1,13 @@
 "use strict"; // run code in ES5 strict mode
 
-var compile = require("nodeclass").compile,
+var nodeclass = require("nodeclass"),
+    compile = nodeclass.compile,
     path = require("path");
 
 var alamidPath = path.resolve(__dirname, "../../");
+
+nodeclass.stdout = function(msg) {
+  //No output in test mode
+};
 
 compile(alamidPath + "/lib", alamidPath + "/compiled");
