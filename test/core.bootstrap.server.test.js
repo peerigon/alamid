82282f479bb2ef7d9978ecd9083978624d8f4bce
meaku <michael.jaser@roomieplanet.de>
2012-06-19 17:58:16 +0200

    - added templates for client-side rendering - rendering client config now - updated bootstrap - added tests for bootstrap

---
+++ test/core.bootstrap.server.test.js
@@ -0,0 +1,47 @@
+"use strict";
+
+var expect = require("expect.js"),
+    rewire = require("rewire"),
+    fs = require("fs"),
+    path = require("path"),
+    bootstrap = rewire("../lib/core/bootstrap.server.js"),
+    generateClientConfig = bootstrap.generateClientConfig,
+    renderBootstrapClientTemplate = bootstrap.renderBootstrapClientTemplate;
+
+describe("bootstrap.server", function() {
+
+    var configMock = {
+        "mode" : "development",
+        "secretServerConfig" : "boobs",
+        "isDev" : true,
+        "paths" : {
+            "bundle" : path.resolve(__dirname, "./core.bootstrap/bundle")
+        }
+    };
+    bootstrap.__set__("config", configMock);
+
+    describe("writeClientConfig", function() {
+        it("should read the config values specified in the config.client file", function() {
+            var returnedClientConfig  = generateClientConfig();
+            expect(returnedClientConfig.mode).to.eql("development");
+            expect(returnedClientConfig.isDev).to.be(true);
+            expect(returnedClientConfig.paths).to.be(undefined);
+            expect(returnedClientConfig.secretServerConfig).to.be(undefined);
+        });
+    });
+
+    describe("renderBootstrapClientTemplate", function() {
+
+        bootstrap.renderBootstrapClientTemplate();
+
+        try{
+            var clientBootstrap = fs.readFileSync("./core.bootstrap/bundle/bootstrap.js", "utf-8");
+        }
+        catch(e) {
+            expect(e).to.be(undefined);
+        }
+
+        expect(clientBootstrap).to.contain('clientConfig = {"mode": "development", "basePath": "", "baseUrl": "", "isDev": true};');
+    });
+});
+
