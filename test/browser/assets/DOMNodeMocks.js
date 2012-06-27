5508f5c61740c640040852cdeeee513af335b061
topa <paul.torka@yahoo.de>
2012-06-27 16:25:16 +0200

    fixed html-syntax-error

--- test/browser/assets/DOMNodeMocks.js
+++ test/browser/assets/DOMNodeMocks.js
@@ -11,9 +11,9 @@ var DOMNodeMocks = DOMNodeMocks || {};
 
             "<div data-node='parent'>" +
                 "<form data-node='child-form' action='?' method='post'>" +
-                    "<input <data-node='child-input-a' type='text' value='a'/>" +
-                    "<input <data-node='child-input-b' type='text' value='b'/>" +
-                    "<input <data-node='child-input-c' type='button' value='c'/>" +
+                    "<input data-node='child-input-a' type='text' value='a'/>" +
+                    "<input data-node='child-input-b' type='text' value='b'/>" +
+                    "<input data-node='child-input-c' type='button' value='c'/>" +
                 "</form>" +
             "</div>";
 
