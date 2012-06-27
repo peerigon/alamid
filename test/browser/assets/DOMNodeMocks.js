bb95c9cc48b8deb49c288ee83bd08f6808d7674d
topa <paul.torka@yahoo.de>
2012-06-27 13:51:33 +0200

    Added DOMNodeMocks which provides - at the moment - a method that returns an form with inputs as DOMElement.

---
+++ test/browser/assets/DOMNodeMocks.js
@@ -0,0 +1,24 @@
+var DOMNodeMocks = DOMNodeMocks || {};
+
+(function (window) {
+    "use strict";
+
+    var incubator = document.createElement("div");
+
+    DOMNodeMocks.getForm = function () {
+
+        incubator.innerHTML =
+
+            "<div data-node='parent'>" +
+                "<form data-node='child-form' action='?' method='post'>" +
+                    "<input <data-node='child-input-a' type='text' value='a'/>" +
+                    "<input <data-node='child-input-b' type='text' value='b'/>" +
+                    "<input <data-node='child-input-c' type='button' value='c'/>" +
+                "</form>" +
+            "</div>";
+
+        return incubator.firstChild;
+
+    };
+
+})(window);
