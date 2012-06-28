08a874c887d3f3ab861d3783f9c915661d574497
meaku <michael.jaser@roomieplanet.de>
2012-06-28 18:29:01 +0200

    added missing folders

---
+++ test/core/collectServices/lib/services/A/SomeOtherClass.class.js
@@ -0,0 +1,16 @@
+"use strict"; // run code in ES5 strict mode
+
+"use strict";
+
+var Class = require("nodeclass").Class;
+
+var SomeOtherClass = new Class({
+
+    "init" : function () {
+        //nothing to do here
+    }
+});
+
+module.exports = SomeOtherClass;
+
+
