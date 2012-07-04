89254d194d18b4eacfc12c94b8a600f9870b2d91
topa <paul.torka@yahoo.de>
2012-07-04 15:40:25 +0200

    hooks compile also ChildDisplayObject.class

--- test/client/nof5.hooks.js
+++ test/client/nof5.hooks.js
@@ -1,12 +1,25 @@
 "use strict";
 
-var path = require("path");
+var path = require("path"),
+    nodeclass = require("nodeclass"),
+    compile = require("nodeclass").compile;
 
-var absoluteCompileTestAlamidPath = path.resolve("../testHelpers/compileTestAlamid.js");
+var absoluteCompileTestAlamidPath = path.resolve("../testHelpers/compileTestAlamid.js"),
+    ChildDisplayObjectLib = __dirname + "/DisplayObject/lib",
+    ChildDisplayObjectCompiled = __dirname + "/DisplayObject/compiled";
 
 exports.before = function () {
+
+    delete require.cache[absoluteCompileTestAlamidPath];
+
+    //Compile alamid lib
     console.time("compile alamid");
     require("../testHelpers/compileTestAlamid.js");
     console.timeEnd("compile alamid");
-    delete(require.cache[absoluteCompileTestAlamidPath]);
+
+    //Compile ChildDisplayObject
+    console.time("compile ChildDisplayObject.class.js");
+    compile(ChildDisplayObjectLib, ChildDisplayObjectCompiled);
+    console.timeEnd("compile ChildDisplayObject.class.js");
+
 };
