f84674050c04ee81dce3dfb2b33fe00d480e85a2
topa <paul.torka@yahoo.de>
2012-07-04 11:42:46 +0200

    hook deletes compileTestAlamid from cache after it was exectued, so that it will be executed each time the test will be run

--- test/client/nof5.hooks.js
+++ test/client/nof5.hooks.js
@@ -1,5 +1,12 @@
 "use strict";
 
+var path = require("path");
+
+var absoluteCompileTestAlamidPath = path.resolve("../testHelpers/compileTestAlamid.js");
+
 exports.before = function () {
+    console.time("compile alamid");
     require("../testHelpers/compileTestAlamid.js");
+    console.timeEnd("compile alamid");
+    delete(require.cache[absoluteCompileTestAlamidPath]);
 };
