246f92f5c1bb27233109aaa786569956f048d28f
topa <paul.torka@yahoo.de>
2012-07-10 16:06:07 +0200

    deleted all hooks as they aren't necessary any more

--- test/client/nof5.hooks.js
+++ test/client/nof5.hooks.js
@@ -1,25 +1,7 @@
 "use strict";
 
-var path = require("path"),
-    nodeclass = require("nodeclass"),
-    compile = require("nodeclass").compile;
-
-var absoluteCompileTestAlamidPath = path.resolve("../testHelpers/compileTestAlamid.js"),
-    ChildDisplayObjectLib = __dirname + "/DisplayObject/lib",
-    ChildDisplayObjectCompiled = __dirname + "/DisplayObject/compiled";
-
-exports.before = function () {
-
-    delete require.cache[absoluteCompileTestAlamidPath];
-
-    //Compile alamid lib
-    console.time("compile alamid");
-    require("../testHelpers/compileTestAlamid.js");
-    console.timeEnd("compile alamid");
-
-    //Compile ChildDisplayObject
-    console.time("compile ChildDisplayObject.class.js");
-    compile(ChildDisplayObjectLib, ChildDisplayObjectCompiled);
-    console.timeEnd("compile ChildDisplayObject.class.js");
+/*
+exports.beforeEach = function () {
 
 };
+*/
