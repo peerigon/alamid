f0648399d307c8bc5d47478c7fe1540cedc27e17
topa <paul.torka@yahoo.de>
2012-07-11 10:55:48 +0200

    Renamed values of data-node-attributes of form's child inputs for better readability

--- test/client/assets/DOMNodeMocks.js
+++ test/client/assets/DOMNodeMocks.js
@@ -11,9 +11,9 @@ var DOMNodeMocks = DOMNodeMocks || {};
     DOMNodeMocks.getFormString = function () {
         return (
             "<form data-node='form' action='?' method='post'>" +
-                "<input data-node='child-input-a' type='text' value='a'/>" +
-                "<input data-node='child-input-b' type='text' value='b'/>" +
-                "<input data-node='child-input-c' type='button' value='c'/>" +
+                "<input data-node='input-a' type='text' value='a'/>" +
+                "<input data-node='input-b' type='text' value='b'/>" +
+                "<input data-node='input-c' type='button' value='c'/>" +
             "</form>"
         );
     };
