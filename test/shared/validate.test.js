be01b8be4accc892fd8f75c24fd14c99f5e60e0a
meaku <michael.jaser@roomieplanet.de>
2012-07-25 15:25:53 +0200

    - renamed validation to validator - added tests for remote validation - updated all other tests depending on those changes

--- test/shared/validate.test.js
+++ test/shared/validate.test.js
@@ -1,10 +1,12 @@
 "use strict";
 
-var expect = require("expect.js");
+var expect = require("expect.js"),
+    rewire = require("rewire"),
+    path = require("path");
 
-var validateModule = require("../../lib/shared/validate.js"),
-    validate = validateModule.validate,
-    localValidation = validateModule.localValidation,
+var validator = require("../../lib/shared/validator.js"),
+    validate = validator.validate,
+    localValidation = validator.localValidation,
     testSchema = require("./Model/schemas/OctocatSchema.js"),
     clientSchema = require("./Model/schemas/OctocatSchema.client.js"),
     serverSchema = require("./Model/schemas/OctocatSchema.server.js"),
@@ -23,7 +25,6 @@ describe("validate", function () {
                 location : "Moon"
             };
         });
-
 
         it("should apply the validators and succeed with the default model data", function (done) {
             localValidation(testSchema, testModel, function(result) {
@@ -71,43 +72,96 @@ describe("validate", function () {
 
     describe("validate", function() {
 
-        var testModel;
+        describe("Server", function() {
 
-        beforeEach(function() {
-            testModel = {
-                name : "SuperCat",
-                age : 12,
-                location : "Moon"
-            };
-        });
+            var testModel,
+                modelUrl;
 
-        it("should validate shared schema and localSchema if sharedSchema passed first", function(done) {
-            validate(sharedSchema, serverSchema, testModel, true, function(result) {
-                expect(result.result).to.be(true);
-                expect(result.shared.result).to.be(true);
-                expect(result.local.result).to.be(true);
-                done();
+            beforeEach(function() {
+                testModel = {
+                    name : "SuperCat",
+                    age : 12,
+                    location : "Moon"
+                };
+                modelUrl = "test";
+            });
+
+            it("should validate shared schema and localSchema if sharedSchema passed first", function(done) {
+                validate(sharedSchema, serverSchema, modelUrl, testModel, true, function(result) {
+                    expect(result.result).to.be(true);
+                    expect(result.shared.result).to.be(true);
+                    expect(result.local.result).to.be(true);
+                    done();
+                });
+            });
+
+            it("should not call the remote validator on the server if sharedSchema passed first", function(done) {
+                validate(sharedSchema, serverSchema, modelUrl, testModel, true, function(result) {
+                    expect(result.result).to.be(true);
+                    expect(result.shared.result).to.be(true);
+                    expect(result.local.result).to.be(true);
+                    expect(result.remote).to.be(undefined);
+                    done();
+                });
+            });
+
+            it("should not pass the validator if local validation fails", function(done) {
+                testModel.age = 99;
+                validate(sharedSchema, serverSchema, modelUrl, testModel, true, function(result) {
+                    expect(result.result).to.be(false);
+                    expect(result.shared.result).to.be(true);
+                    expect(result.local.result).to.be(false);
+                    expect(result.local.fields.age).to.be(false);
+                    done();
+                });
+            });
+
+            it("should not run the localValidation if the shared validation failed", function(done) {
+                testModel.age = 102;
+                validate(sharedSchema, serverSchema, modelUrl, testModel, true, function(result) {
+                    expect(result.result).to.be(false);
+                    expect(result.shared.fields.age).to.be(false);
+                    expect(result.shared.result).to.be(false);
+                    expect(result.local).to.be(undefined);
+                    done();
+                });
             });
         });
 
-        it("should not have the right result", function(done) {
-            testModel.age = 99;
-            validate(sharedSchema, serverSchema, testModel, true, function(result) {
-                expect(result.result).to.be(false);
-                expect(result.shared.result).to.be(true);
-                expect(result.local.result).to.be(false);
-                expect(result.local.fields.age).to.be(false);
-                done();
+        describe("Client", function() {
+
+            var validator,
+                testModel,
+                modelUrl;
+
+            before(function() {
+                validator = rewire(path.resolve(__dirname, "../../lib/shared/validator.js"), false);
+                validator.__set__("config", { isClient : true });
             });
-        });
-        it("should not run the localValidation if the shared validation failed", function(done) {
-            testModel.age = 102;
-            validate(sharedSchema, serverSchema, testModel, true, function(result) {
-                expect(result.result).to.be(false);
-                expect(result.shared.fields.age).to.be(false);
-                expect(result.shared.result).to.be(false);
-                expect(result.local).to.be(undefined);
-                done();
+
+            beforeEach(function(){
+                modelUrl = "test";
+                testModel = {
+                    name : "Octocat",
+                    age : 12,
+                    location : "Anywhere"
+                };
+            });
+
+            it("should call the remote validator on client if shared-validation and local validation worked out", function(done) {
+                var remoteValidationMock = function(schema, modelData, callback) {
+                    callback({ result : true, shared : { result : true}, local : { result : true}});
+                };
+
+                validator.__set__("remoteValidation", remoteValidationMock);
+
+                validator.validate(sharedSchema, serverSchema, modelUrl, testModel, true, function(result) {
+                    expect(result.result).to.be(true);
+                    expect(result.shared.result).to.be(true);
+                    expect(result.local.result).to.be(true);
+                    expect(result.remote.result).to.be(true);
+                    done();
+                });
             });
         });
     });
