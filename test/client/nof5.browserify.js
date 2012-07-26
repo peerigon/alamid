0940be12b5b33c4a60b677f1167af6e9f9f0640c
topa <paul.torka@yahoo.de>
2012-07-26 17:32:01 +0200

    added browserify middleware

--- test/client/nof5.browserify.js
+++ test/client/nof5.browserify.js
@@ -1,13 +1,15 @@
 "use strict";
 
 var nodeclass = require("nodeclass"),
-    browserifyBypass = require("browserify-bypass");
+    browserifyBypass = require("browserify-bypass"),
+    rewire = require("rewire");
 
 exports.use = function useHook() {
 
     return [
         browserifyBypass,
-        nodeclass.bundlers.browserify
+        nodeclass.bundlers.browserify,
+        rewire.browserify
     ];
 
 };
