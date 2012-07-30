02d3ff6d480ef062e963176ee02a608ae6b874b0
meaku <michael.jaser@roomieplanet.de>
2012-07-30 10:38:34 +0200

    - tests for iterateMiddleware - changed iterateMiddleware behavior to call error-callback

--- test/integration/server/websocketTransport.test.js
+++ test/integration/server/websocketTransport.test.js
@@ -36,30 +36,39 @@ describe("WebsocketTransport", function() {
         });
     });
 
+
     describe("#Websocket Requesting", function() {
         before(function(done) {
-            this.browser = new Browser();
+            this.browser = new Browser({ debug : true });
             this.browser
                 .visit("http://localhost:9090/statics/websockettest.html")
                 .then(done, done);
         });
 
         it("should return an not defined error for a service request", function(done) {
-            var pommes = this.browser.evaluate("wsRequestTest('read', '/services/whatever', {});");
-            pommes.success = function(res) {
-                expect(JSON.stringify(res)).to.contain('{"status":"error","message":"(alamid) Request failed for path \'whatever\' with Error: \'No service found for \'read\'');
-                done();
-            };
+            var self = this;
+            this.browser.wait(2000, function(){
+
+
+                var pommes = self.browser.evaluate("wsRequestTest('read', '/services/whatever', {});");
+                pommes.success = function(res) {
+                    expect(JSON.stringify(res)).to.contain('{"status":"error","message":"(alamid) Request failed for path \'whatever\' with Error: \'No service found for \'read\'');
+                    done();
+                };
+            });
         });
 
-        it("should return an not defined error for a validator request", function(done) {
-            var pommes = this.browser.evaluate("wsRequestTest('read', '/validators/whatever', {});");
-            pommes.success = function(res) {
-                expect(JSON.stringify(res)).to.contain('{"status":"error","message":"(alamid) Request failed for path \'whatever\' with Error: ');
-                done();
-            };
-        });
+        /*
+         it("should return an not defined error for a validator request", function(done) {
+         var pommes = this.browser.evaluate("wsRequestTest('read', '/validators/whatever', {});");
+         pommes.success = function(res) {
+         expect(JSON.stringify(res)).to.contain('{"status":"error","message":"(alamid) Request failed for path \'whatever\' with Error: ');
+         done();
+         };
+         });
+         */
     });
+
 
     after(function() {
         serverInstance.kill("SIGHUP");
--- test/server/applyMiddleware.test.js
+++ test/server/applyMiddleware.test.js
@@ -2,10 +2,82 @@
 
 var expect = require("expect.js");
 
+var applyMiddleware = require("../../lib/server/applyMiddleware.js");
+
+function mw1(req, res, next) {
+    console.log("mw1");
+    next();
+}
+
+function mw2(req, res, next) {
+    console.log("mw2");
+    next();
+}
+
 describe("applyMiddleware", function () {
 
-    it("", function () {
+    var mw1, mw2, mw3;
 
+    beforeEach(function() {
+
+        mw1 = function (req, res, next) {
+            console.log("mw1");
+            next();
+        };
+
+        mw2 = function(req, res, next) {
+            console.log("mw2");
+            next();
+        };
+
+        mw3 = function(req, res, next) {
+            console.log("mw3");
+            next();
+        };
     });
 
+    it("should pass the functions thru", function (done) {
+        function mw3(req, res, next) {
+            console.log("mw3");
+            expect(req).not.to.be(undefined);
+            expect(res).not.to.be(undefined);
+            done();
+        }
+
+        var req = {},
+            res = {};
+
+        applyMiddleware([mw1, mw2, mw3], req, res, function(err) {
+            done(new Error("Final error-callback should not be called"));
+        });
+    });
+
+    it("should should call the final error-callback on error", function (done) {
+
+        mw3 = function(req, res, next) {
+            done(new Error("mw3 should not be reached"));
+        };
+
+        mw2 = function(req, res, next) {
+            next(new Error("Random Error"));
+        };
+
+        var req = {},
+            res = {};
+
+        applyMiddleware([mw1, mw2, mw3], req, res, function(err) {
+            done();
+        });
+    });
+
+    it("should reach the final callback", function (done) {
+
+        var req = {},
+            res = {};
+
+        applyMiddleware([mw1, mw2, mw3], req, res, function(err) {
+            expect(err).to.be(null);
+            done();
+        });
+    });
 });
