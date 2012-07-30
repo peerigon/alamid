8fb74bac8423547c0f1887b3bf1653b1e79c5ab7
meaku <michael.jaser@roomieplanet.de>
2012-07-30 10:41:50 +0200

    - sorry jenky! i forgot you don't like the OLG

--- test/server/applyMiddleware.test.js
+++ test/server/applyMiddleware.test.js
@@ -4,16 +4,6 @@ var expect = require("expect.js");
 
 var applyMiddleware = require("../../lib/server/applyMiddleware.js");
 
-function mw1(req, res, next) {
-    console.log("mw1");
-    next();
-}
-
-function mw2(req, res, next) {
-    console.log("mw2");
-    next();
-}
-
 describe("applyMiddleware", function () {
 
     var mw1, mw2, mw3;
@@ -21,24 +11,20 @@ describe("applyMiddleware", function () {
     beforeEach(function() {
 
         mw1 = function (req, res, next) {
-            console.log("mw1");
             next();
         };
 
         mw2 = function(req, res, next) {
-            console.log("mw2");
             next();
         };
 
         mw3 = function(req, res, next) {
-            console.log("mw3");
             next();
         };
     });
 
     it("should pass the functions thru", function (done) {
         function mw3(req, res, next) {
-            console.log("mw3");
             expect(req).not.to.be(undefined);
             expect(res).not.to.be(undefined);
             done();
