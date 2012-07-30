30fc7a0473ae1f7771b59528d2677f741b02e688
topa <paul.torka@yahoo.de>
2012-07-30 18:38:03 +0200

    It is now possible to construct a Collection with models.

---
+++ test/shared/Collection/CollectionExample.class.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var Class = require("nodeclass").Class,
+    Collection = require("../../../lib/shared/Collection.class.js");
+
+var CollectionExample = new Class({
+
+    Extends: Collection,
+
+    /**
+     * Exposes the collections elements.
+     * Use this function to test for example .push() or .set() independently of any other function like .get().
+     *
+     * @return {array}
+     */
+    getElements: function () {
+        return this.Super.getElements();
+    }
+
+});
+
+module.exports = CollectionExample;
+
