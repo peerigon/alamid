2ca4f2c37faa7f1595642bcd10f83eb84037678b
Johannes <johannes.ewald@roomieplanet.de>
2012-08-01 20:23:48 +0200

    - all pageRegistry tests running

---
+++ test/shared/registries/pageRegistry.test.js
@@ -0,0 +1,69 @@
+"use strict"; // run code in ES5 strict mode
+
+var expect = require("expect.js"),
+    pageRegistry = require("../../../lib/shared/registries/pageRegistry.js");
+
+describe("pageRegistry", function () {
+    function bundleA(callback) { callback("A"); }
+    function bundleB(callback) { callback("B"); }
+    function bundleAsync(callback) {
+        setTimeout(function () {
+            callback("Async");
+        }, 0);
+    }
+    function dataLoaderA() {}
+
+    describe(".setPage()", function () {
+        it("should throw no error", function () {
+            pageRegistry.setPage("my/page/A", bundleA, dataLoaderA);
+            pageRegistry.setPage("my/page/B", bundleB);
+            pageRegistry.setPage("my/page/Async", bundleAsync);
+        });
+    });
+    describe(".getPageBundle()", function () {
+        it("should return a method that acts like the sync bundle", function (done) {
+            var wrappedBundle = pageRegistry.getPageBundle("my/page/A");
+
+            wrappedBundle(function callback(bundleName) {
+                expect(bundleName).to.be("A");
+                done();
+            });
+        });
+        it("should return a method that acts like the async bundle", function (done) {
+            var wrappedBundle = pageRegistry.getPageBundle("my/page/Async");
+
+            wrappedBundle(function callback(bundleName) {
+                expect(bundleName).to.be("Async");
+                done();
+            });
+        });
+        it("should return undefined if the pageURL is unknown", function () {
+            expect(pageRegistry.getPageBundle("my/page/C")).to.be(undefined);
+        });
+    });
+    describe(".getPageDataLoader()", function () {
+        it("should return the data loader", function () {
+            expect(pageRegistry.getPageDataLoader("my/page/A")).to.be(dataLoaderA);
+        });
+        it("should return null if the page has no dataLoader", function () {
+            expect(pageRegistry.getPageDataLoader("my/page/B")).to.be(null);
+        });
+        it("should return undefined if the pageURL is unknown", function () {
+            expect(pageRegistry.getPageDataLoader("my/page/C")).to.be(undefined);
+        });
+    });
+    describe(".getPageClass()", function () {
+        it("should return null if the bundle hasn't been executed before", function () {
+            expect(pageRegistry.getPageClass("my/page/B")).to.be(null);
+        });
+        it("should return the page class if the bundle has been executed", function () {
+            var bundle = pageRegistry.getPageBundle("my/page/B");
+
+            bundle(function () {});
+            expect(pageRegistry.getPageClass("my/page/B")).to.be("B");
+        });
+        it("should return undefined if the pageURL is unknown", function () {
+            expect(pageRegistry.getPageClass("my/page/C")).to.be(undefined);
+        });
+    });
+});
