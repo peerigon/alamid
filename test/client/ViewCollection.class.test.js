07c900fb2bff82da400f91509b6d2b8fe97d689f
topa <paul.torka@yahoo.de>
2012-08-02 11:47:15 +0200

    Current ViewCollection state

--- test/client/ViewCollection.class.test.js
+++ test/client/ViewCollection.class.test.js
@@ -5,24 +5,53 @@ var expect = require("expect.js"),
     DisplayObject = require("../../lib/client/DisplayObject.class.js"),
     ViewCollection = require("../../lib/client/ViewCollection.class.js"),
     ViewCollectionExampleWithTemplate = require("./mocks/ViewCollectionExampleWithTemplate.class.js"),
-    View = require("../../lib/client/View.class.js"),
-    ViewExampleWithTemplate = require("./mocks/ViewExampleWithTemplate.class.js"),
-    ModelCollection = require("../../lib/shared/ModelCollection.class.js");
+    CarLiElementView = require("./mocks/CarLiElementView.class.js"),
+    ModelCollection = require("../../lib/shared/ModelCollection.class.js"),
+    CarModel = require("./mocks/models/CarModel.class.js");
 
 describe("ViewCollection", function () {
 
-    var ulString,
-        ul,
-        $ul,
-        viewCollection;
+    var viewCollectioNode,
+        $viewCollectioNode,
+        viewCollection,
+        audi,
+        bmw,
+        daimler,
+        porsche,
+        cars,
+        carCollection;
 
     beforeEach(function () {
 
-        ulString = DOMNodeMocks.getUlString();
-        ul = DOMNodeMocks.getUl();
-        $ul = jQuery(ul);
-        viewCollection = new ViewCollectionExampleWithTemplate(ViewExampleWithTemplate);
-
+        viewCollection = new ViewCollectionExampleWithTemplate(CarLiElementView);
+        viewCollectioNode = viewCollection.getNode();
+        $viewCollectioNode = jQuery(viewCollectioNode);
+        audi = new CarModel();
+        audi.set({
+            "manufactor": "Audi",
+            "model": "A6",
+            "yoc": new Date(2010)
+        });
+        bmw = new CarModel();
+        bmw.set({
+            "manufactor": "BMW",
+            "model": "3",
+            "yoc": new Date(2011)
+        });
+        daimler = new CarModel();
+        daimler.set({
+            "manufactor": "Daimler",
+            "model": "E",
+            "yoc": new Date(2008)
+        });
+        porsche = new CarModel();
+        porsche.set({
+            "manufactor": "Porsche",
+            "model": "911",
+            "yoc": new Date(1986)
+        });
+        cars = [audi, bmw, daimler];
+        carCollection = new ModelCollection(CarModel, cars);
     });
 
     describe(".construct()", function () {
@@ -31,9 +60,67 @@ describe("ViewCollection", function () {
            expect(is(viewCollection).instanceOf(DisplayObject)).to.be.ok();
         });
 
-        //it("should be possible to construct ")
+        it("should be possible to set a template", function (done) {
+            viewCollection = new ViewCollection(CarLiElementView, DOMNodeMocks.getOlString());
+            done();
+        });
+
+        it("should be possible to override a declared template", function () {
+            viewCollection = new ViewCollectionExampleWithTemplate(CarLiElementView, DOMNodeMocks.getOlString());
+
+            expect(viewCollection.getNode().toString()).to.be.equal(DOMNodeMocks.getOl().toString());
+        });
+
+        it("should throw an Error if a template was passed that does not include a node with 'data-node=\"views\"'", function () {
+            expect(function () {
+                viewCollection = new ViewCollection(CarLiElementView, "<ul></ul>");
+            }).to.throwError();
+        });
 
     });
+
+    describe(".bind()", function () {
+
+        beforeEach(function () {
+            viewCollection.bind(carCollection);
+        });
+
+        it("should throw an Error if now instance of ModelCollection was given", function () {
+            expect(function () {
+                viewCollection.bind({});
+            }).to.throwError();
+        });
+
+        it("should render for each Model in ModelCollection a View", function () {
+            expect($viewCollectioNode.find("li").length).to.be.equal(carCollection.size());
+        });
+
+        it("should bind each Model in ModelCollection to a View", function () {
+            var $liElements;
+
+            carCollection.each(function changeManufactor(model) {
+                model.set("manufactor", "Toyota");
+            });
+
+            $liElements = $viewCollectioNode.find("li");
+
+            $liElements.each(function checkManufactor(index, liElement) {
+                expect(jQuery(liElement).find("[data-node='manufactor']").text()).to.be.equal("Toyota");
+            });
+        });
+
+        it("should create new Views for each Model which was added to ModelCollection", function () {
+            carCollection.push(porsche);
+            //expect($viewCollectioNode.find("li").length).to.be.equal(carCollection.size());
+        });
+
+        it("should return a reference to itself", function () {
+           expect(viewCollection.bind(carCollection)).to.be.equal(viewCollection);
+        });
+
+    });
+
+
 
 
 });
