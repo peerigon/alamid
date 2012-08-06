8dd8041f28cc382fa655ed7016d5baa2b3c55dc2
Johannes <johannes.ewald@roomieplanet.de>
2012-08-06 17:21:18 +0200

    - Added setSubPage to the PageMock

--- test/client/mocks/PageMock.class.js
+++ test/client/mocks/PageMock.class.js
@@ -1,8 +1,10 @@
 "use strict"; // run code in ES5 strict mode
 
-var Class = require("nodeclass").Class;
+var Class = require("nodeclass").Class,
+    EventEmitter = require("../../../lib/shared/EventEmitter.class.js");
 
 var PageMock = new Class({
+    Extends: EventEmitter,
     subPage: null,
     params: null,
     emitted: [],
@@ -11,6 +13,11 @@ var PageMock = new Class({
     },
     emit: function () {
         this.emitted.push(arguments);
+        this.Super.emit.apply(this.Super, arguments);
+    },
+    setSubPage: function (subPage) {
+        subPage.emit("append");
+        this.subPage = subPage;
     }
 });
 
