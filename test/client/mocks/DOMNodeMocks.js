9118337f5750cc8df9006edc7a2a20fdfdc77ad8
topa <paul.torka@yahoo.de>
2012-08-10 12:28:02 +0200

    Refactored DOMNodeMocks in Node-Module-Style

--- test/client/mocks/DOMNodeMocks.js
+++ test/client/mocks/DOMNodeMocks.js
@@ -1,14 +1,16 @@
-var DOMNodeMocks = DOMNodeMocks || {};
+"use strict"
 
-(function (window) {
-    "use strict";
+var DOMNodeMocks = {
 
-    var incubator = document.createElement("div");
+    /**
+     * @type {Node}
+     */
+    __incubator: document.createElement("div"),
 
     /**
      * @return {String}
      */
-    DOMNodeMocks.getFormString = function () {
+    getFormString: function () {
         return (
             "<form data-node='form' action='?' method='post'>" +
                 "<input data-node='input-a' type='text' value='a'/>" +
@@ -16,69 +18,81 @@ var DOMNodeMocks = DOMNodeMocks || {};
                 "<input data-node='input-c' type='button' value='c'/>" +
             "</form>"
         );
-    };
+    },
 
     /**
      * @return {Node}
      */
-    DOMNodeMocks.getForm = function () {
+    getForm: function () {
+        this.__incubator.innerHTML = this.getFormString();
 
-        incubator.innerHTML = this.getFormString();
-
-        return incubator.firstChild;
-    };
+        return this.__incubator.firstChild;
+    },
 
     /**
      * @return {String}
      */
-    DOMNodeMocks.getSubmitButtonString = function () {
+    getSubmitButtonString: function () {
         return "<input data-node='submit-button' type='submit' value='submit'/>";
-    };
+    },
 
     /**
      * @return {Node}
      */
-    DOMNodeMocks.getSubmitButton = function () {
+    getSubmitButton: function () {
+        this.__incubator.innerHTML = this.getSubmitButtonString();
 
-        incubator.innerHTML = this.getSubmitButtonString();
-
-        return incubator.firstChild;
-    };
+        return this.__incubator.firstChild;
+    },
 
     /**
      * @return {String}
      */
-    DOMNodeMocks.getUlString = function () {
+    getUlString: function () {
         return "<ul data-node='views'></ul>";
-    };
+    },
 
     /**
      * @return {Node}
      */
-    DOMNodeMocks.getUl = function () {
+    getUl: function () {
+        this.__incubator.innerHTML = this.getUlString();
 
-        incubator.innerHTML = this.getUlString();
-
-        return incubator.firstChild;
-    };
+        return this.__incubator.firstChild;
+    },
 
     /**
      * @return {String}
      */
-    DOMNodeMocks.getOlString = function () {
+    getOlString: function () {
         return "<ul data-node='views'></ul>";
-    };
+    },
 
     /**
      * @return {Node}
      */
-    DOMNodeMocks.getOl = function () {
+    getOl: function () {
+        this.__incubator.innerHTML = this.getOlString();
 
-        incubator.innerHTML = this.getOlString();
+        return this.__incubator.firstChild;
+    },
 
-        return incubator.firstChild;
-    };
+    /**
+     * @return {String}
+     */
+    getAString: function () {
 
+    },
 
+    /**
+     * @return {Node}
+     */
+    getA: function () {
+        this.__incubator.innerHTML = this.getAString();
 
-})(window);
+        return this.__incubator.firstChild;
+    }
+
+};
+
+module.exports = DOMNodeMocks;
