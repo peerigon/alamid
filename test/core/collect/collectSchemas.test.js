32822b7caed7cad3f560da267f5aded925b9e0e1
meaku <michael.jaser@roomieplanet.de>
2012-08-13 10:55:54 +0200

    - fixed collectSchemas.test.js

--- test/core/collect/collectSchemas.test.js
+++ test/core/collect/collectSchemas.test.js
@@ -5,7 +5,7 @@ var expect = require("expect.js"),
     nodeclass = require("nodeclass"),
     path = require("path"),
     collectSchemas,
-    modelsFolder = path.resolve(__dirname, "../exampleApp/app/models");
+    modelsFolder = path.resolve(__dirname, "./collectModels/app/models");
 
 nodeclass.registerExtension();
 
@@ -13,14 +13,13 @@ nodeclass.stdout = function(msg) {
     //No output in test mode
 };
 
-/*
 describe("collectSchemas", function () {
 
     afterEach(function () {
         rewire.reset();
     });
 
-    it("should collect appropriately, extend schemas and return the defined schemas", function (done) {
+    it("should collect appropriately, extend schemas and return paths to the defined schemas", function () {
 
         var expectedSchemas = {
             server: {},
@@ -28,55 +27,50 @@ describe("collectSchemas", function () {
             shared: {}
         };
 
-        function onCollectSchemasEnd(err, schemas) {
-            expect(err).to.be(null);
-            expect(schemas.server).to.only.have.keys(Object.keys(expectedSchemas.server));
-            expect(schemas.client).to.only.have.keys(Object.keys(expectedSchemas.client));
-            expect(schemas.shared).to.only.have.keys(Object.keys(expectedSchemas.shared));
-            expect(schemas.server.blogpost).to.be.an("object");
-
-            expect(schemas.server["blogpost/comment"]).to.be.an("object");
-            expect(schemas.shared["blogpost/comment"]).to.be.an("object");
-
-            //required was overwritten by server schema
-            expect(schemas.shared.blogpost.email.required).to.be(true);
-            expect(schemas.server.blogpost.email.required).to.be(false);
-            expect(schemas.client.blogpost.email.required).to.be(true);
-
-            //new attribute for client only
-            expect(schemas.client.blogpost.saved).not.to.be(undefined);
-            expect(schemas.shared.blogpost.saved).to.be(undefined);
-            expect(schemas.server.blogpost.saved).to.be(undefined);
-
-            done();
-        }
-
         expectedSchemas.server.blogpost = true;
-        expectedSchemas.server.user = true;
         expectedSchemas.server["blogpost/comment"] = true;
 
         expectedSchemas.client.blogpost = true;
-        expectedSchemas.client.user= true;
         expectedSchemas.client["blogpost/comment"] = true;
 
         expectedSchemas.shared.blogpost = true;
-        expectedSchemas.shared.user= true;
         expectedSchemas.shared["blogpost/comment"] = true;
 
-        collectSchemas = rewire("../../lib/core/collectSchemas.js", false);
-        collectSchemas (modelsFolder, onCollectSchemasEnd);
+        collectSchemas = rewire("../../../lib/core/collect/collectSchemas.js", false);
+        var schemas = collectSchemas (modelsFolder);
+
+        expect(schemas.server).to.only.have.keys(Object.keys(expectedSchemas.server));
+        expect(schemas.client).to.only.have.keys(Object.keys(expectedSchemas.client));
+        expect(schemas.shared).to.only.have.keys(Object.keys(expectedSchemas.shared));
+        expect(schemas.server.blogpost).to.eql(path.resolve(__dirname, "./collectModels/app/models/BlogPost/BlogPostSchema.js"));
+
+        /*
+         //COPY THESE tests to BOOTSTRAP.server test
+         expect(schemas.shared.blogpost.email.required).to.be(true);
+         expect(schemas.server.blogpost.email.required).to.be(false);
+         expect(schemas.client.blogpost.email.required).to.be(true);
+
+
+         //new attribute for client only
+         expect(schemas.client.blogpost.saved).not.to.be(undefined);
+         expect(schemas.shared.blogpost.saved).to.be(undefined);
+         expect(schemas.server.blogpost.saved).to.be(undefined);
+         */
+
+        expect(schemas.server["blogpost/comment"]).to.eql(path.resolve(__dirname, "./collectModels/app/models/BlogPost/Comment/CommentSchema.js"));
+        expect(schemas.shared["blogpost/comment"]).to.be.eql(path.resolve(__dirname, "./collectModels/app/models/BlogPost/Comment/CommentSchema.js"));
     });
 
 
-    it("should fail on non existing folders", function (done) {
+    it("should fail on non existing folders", function () {
 
-        function onCollectModelsError(err) {
-            expect(err instanceof Error).to.be(true);
-            done();
+        collectSchemas = rewire("../../../lib/core/collect/collectServices.js", false);
+
+        try{
+            collectSchemas(__dirname+"/non/existing/folder/");
         }
-
-        collectSchemas = rewire("../../lib/core/collectServices.js", false);
-        collectSchemas(__dirname+"/non/existing/folder/" , onCollectModelsError);
+        catch(err) {
+            expect(err instanceof Error).to.be(true);
+        }
     });
 });
-*/
