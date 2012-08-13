316a4ac5c273a8677672dd08572b8694c408549f
meaku <michael.jaser@roomieplanet.de>
2012-08-13 10:48:26 +0200

    - fixed collectModels.test

--- test/core/collect/collectModels.test.js
+++ test/core/collect/collectModels.test.js
@@ -13,14 +13,14 @@ nodeclass.registerExtension();
 nodeclass.stdout = function(msg) {
     //No output in test mode
 };
-/*
+
 describe("collectModels", function () {
 
     afterEach(function () {
         rewire.reset();
     });
 
-    it("should collect appropriately and return required modules for server-services", function () {
+    it("should collect appropriately and return urls and paths for the services", function () {
 
         var expectedModels = {
             server: {},
@@ -31,24 +31,23 @@ describe("collectModels", function () {
         expectedModels.server["blogpost/comment"] = true;
         expectedModels.client.blogpost = true;
 
-        collectModels = rewire("../../lib/core/collectModels.js", false);
+        collectModels = rewire("../../../lib/core/collect/collectModels.js", false);
         var models = collectModels(servicesFolder);
 
         expect(models.server).to.only.have.keys(Object.keys(expectedModels.server));
         expect(models.client).to.only.have.keys(Object.keys(expectedModels.client));
-        expect(models.server.blogpost).to.be.an("object");
-        expect(models.server["blogpost/comment"]).to.be.an("object");
+        expect(models.server.blogpost).to.eql(path.resolve(__dirname + "/collectModels/app/models/BlogPost/BlogPost.server.class.js"));
+        expect(models.server["blogpost/comment"]).to.eql(path.resolve(__dirname + "/collectModels/app/models/BlogPost/Comment/Comment.server.class.js"));
     });
 
-    it("should fail on non existing folders", function (done) {
+    it("should fail on non existing folders", function () {
 
-        function onCollectModelsError(err) {
-            expect(err instanceof Error).to.be(true);
-            done();
+        try{
+            collectModels = rewire("../../../lib/core/collect/collectModels.js", false);
+            collectModels(__dirname+"/non/existing/folder/" , onCollectModelsError);
         }
-
-        collectModels = rewire("../../lib/core/collectServices.js", false);
-        collectModels(__dirname+"/non/existing/folder/" , onCollectModelsError);
+        catch(err) {
+            expect(err instanceof Error).to.be(true);
+        }
     });
 });
-    */
