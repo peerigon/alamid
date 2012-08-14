83dc7107dcfa50d6f6f3848f87fbd1287e7d3f54
topa <paul.torka@yahoo.de>
2012-08-14 14:45:03 +0200

    creating Class without define methods until Bug with nodeclass gets fixed

--- test/client/mocks/PageExample.class.js
+++ test/client/mocks/PageExample.class.js
@@ -1,11 +1,37 @@
 "use strict";
 
-var Page = require("../../../lib/client/Page.class.js");
+var Class = require("nodeclass").Class,
+    Page = require("../../../lib/client/Page.class.js");
 
 console.log(Page.define);
 console.log(Page.$define);
 
+/*
 var PageExample = Page.define({
+
+    /**
+     * Exposes protected ._getParams()
+     * @return {object}
+     */
+    /*
+    getParams: function () {
+        return this._getParams();
+    },
+
+    /**
+     * Exposes ._isDisposed()
+     * @return {boolean}
+     */
+/*
+    isDisposed: function () {
+        return this._isDisposed();
+    },
+
+    $template: "<div data-node='page'></div>"
+
+});
+*/
+var PageExample = new Class({
 
     /**
      * Exposes protected ._getParams()
@@ -26,5 +52,6 @@ var PageExample = Page.define({
     $template: "<div data-node='page'></div>"
 
 });
+
 
 module.exports = PageExample;
