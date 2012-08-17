c30b3d27226238046a1ae4b40d9a39dd4748312c
topa <paul.torka@yahoo.de>
2012-08-17 13:52:40 +0200

    - added hooks for webpack

---
+++ test/client/nof5.webpack.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var resolveFilename = require("../../lib/core/bundle/resolveFilename.js"),
+    nodeclassLoader = require("nodeclass").bundlers.webpack,
+    path = require("path");
+
+exports.use = function () {
+
+    return {
+        context: path.resolve(__dirname + "../../"),
+        includeFilenames: true,
+        debug: true,
+        extensions: ["", ".client.js", ".js"],
+        resolve: {
+            loaders: [
+                nodeclassLoader,
+                { test: /\.html$/i, loader: "raw" }
+            ],
+            postprocess: {
+                normal: [resolveFilename]
+            }
+        }
+    };
+
+};
