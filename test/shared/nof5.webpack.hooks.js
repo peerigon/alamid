728dcd7a2483c3448714b98c4fc0e289d8443344
topa <paul.torka@yahoo.de>
2012-09-14 15:07:08 +0200

    - added webpack hooks to shared

---
+++ test/shared/nof5.webpack.hooks.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var resolveFilename = require("../../lib/core/bundle/resolveFilename.js"),
+    nodeclassLoader = require("nodeclass").bundlers.webpack,
+    rewireWebpackExtension = require("rewire").bundlers.webpack,
+    path = require("path");
+
+exports.use = function () {
+
+    var options = {
+        context: path.resolve(__dirname, "../../"),
+        includeFilenames: true,
+        debug: true,
+        extensions: ["", ".client.js", ".js"],
+        resolve: {
+            loaders: [
+                nodeclassLoader,
+                { test: /\.html$/i, loader: "raw" }
+            ],
+            postprocess: {
+                normal: [resolveFilename]
+            }
+        }
+    };
+
+    rewireWebpackExtension(options);
+
+    return options;
+
+};
