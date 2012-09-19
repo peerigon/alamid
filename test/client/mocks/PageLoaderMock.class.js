f15ab47fcaf28aab1c436d6b1fae3b1310673c38
topa <paul.torka@yahoo.de>
2012-09-19 09:47:28 +0200

    - added docs

--- test/client/mocks/PageLoaderMock.class.js
+++ test/client/mocks/PageLoaderMock.class.js
@@ -3,19 +3,49 @@
 var Class = require("nodeclass").Class;
 
 var PageLoaderMock = new Class({
+
+    /**
+     * @type {Function}
+     */
     $instance: null,
+
+    /**
+     * @type {Function}
+     */
     callback: null,
+
+    /**
+     * @type {Array}
+     */
     pageURLs: null,
+
+    /**
+     * @type {Object}
+     */
     params: null,
+
+    /**
+     * @type {Boolean}
+     */
     cancelled: false,
+
+    /**
+     * @param {Array} pageURLs
+     */
     init: function (pageURLs) {
         this.pageURLs = pageURLs;
         PageLoaderMock.instance = this.Instance;
     },
+
+    /**
+     * @param {Object} params
+     * @param {Function} callback
+     */
     load: function (params, callback) {
         this.params = params;
         this.callback = callback;
     },
+
     cancel: function () {
         this.cancelled = true;
     }
