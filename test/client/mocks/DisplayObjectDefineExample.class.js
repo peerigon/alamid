a9a33c5914a90d5f10746c549b4012c44f5caf03
topa <paul.torka@yahoo.de>
2012-09-21 20:10:18 +0200

    - forgot to commit this mock that is needed to test DisplayObjects' define #74

---
+++ test/client/mocks/DisplayObjectDefineExample.class.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var DisplayObject = require("../../../lib/client/DisplayObject.class.js");
+
+var DisplayObjectDefineExample = DisplayObject.define({
+
+    /**
+     * @type {Function}
+     */
+    __done: null,
+
+    /**
+     * @param {Function} done
+     */
+    init: function (done) {
+
+        var template = "<div data-role='page'></div>";
+
+        this.Super(template);
+
+        this.__done = done;
+    },
+
+    executeDone: function () {
+        this.__done();
+    }
+
+});
+
+module.exports = DisplayObjectDefineExample;
