affdc1c52c6157573129373032a6ce2398c02d48
meaku <michael.jaser@roomieplanet.de>
2012-09-25 19:18:54 +0200

    - changed resolveFilename to handle alamid requires differently - looking for client files for core- and server-requires - removed obsolete files - moved client files - closes #65

--- test/core/bundle/resolveFilename.test.js
+++ test/core/bundle/resolveFilename.test.js
@@ -1,59 +1,90 @@
 "use strict"; // run code in ES5 strict mode
 
 var expect = require("expect.js"),
+    path = require("path"),
     resolveFilename = require("../../../lib/core/bundle/resolveFilename.js");
 
-var testFolder = "someLoader.js!" + __dirname + "/resolveFilename/";
+var testFolder = "someLoader.js!" + __dirname + "/resolveFilename/",
+    alamidFolder = path.resolve(__dirname, "../../../lib/");
 
 describe("resolveFilename", function() {
 
-    beforeEach(function () {
-        resolveFilename.statCache = {};
+    describe("alamid requires", function() {
+
+        beforeEach(function () {
+            resolveFilename.statCache = {};
+        });
+
+        it("should handle a require from the alamid server folder", function(done) {
+            resolveFilename(alamidFolder + "/server/environment.server.js", function(err, resolvedFilename) {
+                expect(err).to.be(null);
+                expect(resolvedFilename).to.eql(alamidFolder + "/client/environment.client.js");
+                done();
+            });
+        });
+
+        it("should handle a require from the alamid core folder", function(done) {
+            resolveFilename(alamidFolder + "/core/config.server.js", function(err, resolvedFilename) {
+                expect(err).to.be(null);
+                expect(resolvedFilename).to.eql(alamidFolder + "/client/config.client.js");
+                done();
+            });
+        });
+
     });
 
-    it("should return a client-file as replacement for a server-file if existent", function(done) {
-        resolveFilename(testFolder + "config.server.js", function(err, resolvedFilename) {
-            expect(err).to.be(null);
-            expect(resolvedFilename).to.eql(testFolder + "config.client.js");
-            done();
+    describe("app requires", function() {
+
+        beforeEach(function () {
+            resolveFilename.statCache = {};
+        });
+
+        it("should return a client-file as replacement for a server-file if existent", function(done) {
+            resolveFilename(testFolder + "config.server.js", function(err, resolvedFilename) {
+                expect(err).to.be(null);
+                expect(resolvedFilename).to.eql(testFolder + "config.client.js");
+                done();
+            });
+        });
+
+        it("should return a client-file as replacement for a server-file if existent (for class files)", function(done) {
+            resolveFilename(testFolder + "logger.server.class.js", function(err, resolvedFilename) {
+                expect(err).to.be(null);
+                expect(resolvedFilename).to.eql(testFolder + "logger.client.class.js");
+                done();
+            });
+        });
+
+        it("should return shared files without env-specific file-ending", function(done) {
+            resolveFilename(testFolder + "index.js", function(err, resolvedFilename) {
+                expect(err).to.be(null);
+                expect(resolvedFilename).to.eql(testFolder + "index.js");
+                done();
+            });
+        });
+
+        it("should return shared files if existent (.class ending)", function(done) {
+            resolveFilename(testFolder + "modelCache.server.class.js", function(err, resolvedFilename) {
+                expect(err).to.be(null);
+                expect(resolvedFilename).to.eql(testFolder + "modelCache.class.js");
+                done();
+            });
+        });
+
+        it("should fail if a server-file is being required without client or shared replacement", function(done) {
+            resolveFilename(testFolder + "serverOnly.server.js", function(err, resolvedFilename) {
+                expect(err).to.be.an(Error);
+                done();
+            });
+        });
+
+        it("should fail if the file doesn't exist", function (done) {
+            resolveFilename(testFolder + "abc", function (err) {
+                expect(err).to.be.an(Error);
+                done();
+            });
         });
     });
 
-    it("should return a client-file as replacement for a server-file if existent (for class files)", function(done) {
-        resolveFilename(testFolder + "logger.server.class.js", function(err, resolvedFilename) {
-            expect(err).to.be(null);
-            expect(resolvedFilename).to.eql(testFolder + "logger.client.class.js");
-            done();
-        });
-    });
 
-    it("should return shared files without env-specific file-ending", function(done) {
-        resolveFilename(testFolder + "index.js", function(err, resolvedFilename) {
-            expect(err).to.be(null);
-            expect(resolvedFilename).to.eql(testFolder + "index.js");
-            done();
-        });
-    });
-
-    it("should return shared files if existent (.class ending)", function(done) {
-        resolveFilename(testFolder + "modelCache.server.class.js", function(err, resolvedFilename) {
-            expect(err).to.be(null);
-            expect(resolvedFilename).to.eql(testFolder + "modelCache.class.js");
-            done();
-        });
-    });
-
-    it("should fail if a server-file is being required without client or shared replacement", function(done) {
-        resolveFilename(testFolder + "serverOnly.server.js", function(err, resolvedFilename) {
-            expect(err).to.be.an(Error);
-            done();
-        });
-    });
-
-    it("should fail if the file doesn't exist", function (done) {
-        resolveFilename(testFolder + "abc", function (err) {
-            expect(err).to.be.an(Error);
-            done();
-        });
-    });
 });
