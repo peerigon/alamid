e74cc8ed4753591f011ca182ca3899e0b004bb4f
Johannes <johannes.ewald@roomieplanet.de>
2012-09-25 11:55:20 +0200

    - Fixed bundler

--- test/core/bundle/createBundle/app/pages/blog/blogPageDataLoader.js
+++ test/core/bundle/createBundle/app/pages/blog/blogPageDataLoader.js
@@ -1,3 +1,5 @@
 "use strict"; // run code in ES5 strict mode
 
-module.exports = function blogPageDataLoader() {};
+module.exports = function blogPageDataLoader(params, callback) {
+    callback(null, {});
+};
