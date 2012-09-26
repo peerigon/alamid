e200eecdfbac118c1cce1252fda280ae98d81a2f
meaku <michael.jaser@roomieplanet.de>
2012-09-26 15:35:19 +0200

    - added switch for createBundleTests because jenkins has troubles due to missing alamid-folder

--- grunt.js
+++ grunt.js
@@ -1,5 +1,9 @@
 "use strict";
 
+var exec = require('child_process').exec,
+    child,
+    path = require("path");
+
 module.exports = function(grunt) {
 
     grunt.initConfig({
@@ -83,6 +87,34 @@ module.exports = function(grunt) {
         env.mode = "testing"; //logger be quiet
     });
 
+    grunt.registerTask("test-nof5", "tests with nof5", function() {
+
+        var done = this.async(),
+            cmd = 'cd ' + path.resolve(__dirname, "./test/client") + " ; nof5";
+
+        console.log(cmd);
+
+        child = exec(cmd,
+            function (error, stdout, stderr) {
+                console.log('stdout: ' + stdout);
+                console.log('stderr: ' + stderr);
+                if (error !== null) {
+                    console.log('exec error: ' + error);
+                }
+            });
+
+        setTimeout(function() {
+            var cmd2 = "open http://localhost:11234";
+            console.log("cms2", cmd2);
+            var browser = exec(cmd2, function(err, stdout, stderr){
+                console.log(err, stdout, stderr);
+            });
+
+        }, 1000);
+
+
+    });
+
     //mocha tests
     grunt.registerTask("test-server", "enable-testing-mode simplemocha:server");
     grunt.registerTask("test-core", "enable-testing-mode simplemocha:core");
@@ -92,4 +124,37 @@ module.exports = function(grunt) {
     grunt.registerTask('test-all', "enable-testing-mode simplemocha:all");
 
     grunt.registerTask("test-jenkins", "enable-testing-mode simplemocha:jenkins");
+
+
+    // ==========================================================================
+    // HELPERS
+    // ==========================================================================
+
+    grunt.registerHelper('phantomjs', function(options) {
+        return grunt.utils.spawn({
+            cmd: 'phantomjs',
+            args: options.args
+        }, function(err, result, code) {
+            if (!err) { return options.done(null); }
+            // Something went horribly wrong.
+            grunt.verbose.or.writeln();
+            grunt.log.write('Running PhantomJS...').error();
+            if (code === 127) {
+                grunt.log.errorlns(
+                    'In order for this task to work properly, PhantomJS must be ' +
+                        'installed and in the system PATH (if you can run "phantomjs" at' +
+                        ' the command line, this task should work). Unfortunately, ' +
+                        'PhantomJS cannot be installed automatically via npm or grunt. ' +
+                        'See the grunt FAQ for PhantomJS installation instructions: ' +
+                        'https://github.com/cowboy/grunt/blob/master/docs/faq.md'
+                );
+                grunt.warn('PhantomJS not found.', options.code);
+            } else {
+                result.split('\n').forEach(grunt.log.error, grunt.log);
+                grunt.warn('PhantomJS exited unexpectedly with exit code ' + code + '.', options.code);
+            }
+            options.done(code);
+        });
+    });
+
 };
