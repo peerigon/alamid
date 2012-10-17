9333c7d8e9056a120336a766961b43e23884b16f
meaku <michael.jaser@roomieplanet.de>
2012-10-17 12:04:08 +0200

    - made applyMiddleware aware of res.end - applyMiddleware returns and stops executing if res.end was called - related to #72

--- test/server/applyMiddleware.test.js
+++ test/server/applyMiddleware.test.js
@@ -2,7 +2,8 @@
 
 var expect = require("expect.js");
 
-var applyMiddleware = require("../../lib/server/applyMiddleware.js");
+var applyMiddleware = require("../../lib/server/applyMiddleware.js"),
+    Response = require("../../lib/server/request/Response.class.js");
 
 describe("applyMiddleware", function () {
 
@@ -31,7 +32,7 @@ describe("applyMiddleware", function () {
         }
 
         var req = {},
-            res = {};
+            res = new Response();
 
         applyMiddleware([mw1, mw2, mw3], req, res, function(err) {
             done(new Error("Final error-callback should not be called"));
@@ -49,7 +50,7 @@ describe("applyMiddleware", function () {
         };
 
         var req = {},
-            res = {};
+            res = new Response();
 
         applyMiddleware([mw1, mw2, mw3], req, res, function(err) {
             done();
@@ -59,11 +60,33 @@ describe("applyMiddleware", function () {
     it("should reach the final callback", function (done) {
 
         var req = {},
-            res = {};
+            res = new Response();
 
         applyMiddleware([mw1, mw2, mw3], req, res, function(err) {
             expect(err).to.be(null);
             done();
         });
     });
+
+    it("should end the request if res.end was called", function (done) {
+
+        var resData = { status : "success", data : { da : "ta" }};
+
+        mw3 = function(req, res, next) {
+            done(new Error("mw3 should not be reached"));
+        };
+
+        mw2 = function(req, res, next) {
+            res.end(resData);
+        };
+
+        var req = {},
+            res = new Response();
+
+        applyMiddleware([mw1, mw2, mw3], req, res, function(err) {
+            expect(res.getResBody()).to.eql({ status : "success", data : { da : "ta" }});
+            done();
+        });
+    });
+
 });
