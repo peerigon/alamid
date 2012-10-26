2a21c7674f454729570b4fe538da3502c0a81d1c
topa <paul.torka@yahoo.de>
2012-10-26 14:31:09 +0200

    - update to jQuery 1.8.2 - tidy of jQuery-test

--- test/client/helpers/jQuery.test.js
+++ test/client/helpers/jQuery.test.js
@@ -1,16 +1,16 @@
 "use strict";
 
 var expect = require("expect.js"),
-    jQuery = require("../../../lib/client/helpers/jQuery.js");
+    AlamidjQuery = require("../../../lib/client/helpers/jQuery.js");
 
 describe("jQuery", function () {
 
-    it("should be the jQuery JavaScript library", function () {
-        expect(jQuery.expando.search(/alamidjQuery/)).to.equal(0);
+    it("should be jQuery JavaScript library", function () {
+        expect(AlamidjQuery.expando.search(/alamidjQuery/)).to.equal(0);
     });
 
     it("should not be attached to the window object", function () {
-        expect(jQuery).to.not.equal(window.jQuery);
+        expect(AlamidjQuery).to.not.equal(window.jQuery);
     });
 
 });
