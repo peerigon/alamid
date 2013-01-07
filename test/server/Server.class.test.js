5ba6c2718eb725f367a520d9454f6929faf002b3
miku <michael.jaser@peerigon.com>
2013-01-07 18:07:49 +0100

    - changed binding for service-calls - changes due to the new alamid-class system

--- test/server/Server.class.test.js
+++ test/server/Server.class.test.js
@@ -1,11 +1,34 @@
 "use strict";
 
-var expect = require("expect.js");
+var expect = require("expect.js"),
+    path = require("path");
+
 var Server = require("../../lib/server/Server.class.js");
 
 describe("Server", function () {
 
+    /*
+    describe("#bootstrap", function() {
 
+        var server;
 
+        beforeEach(function() {
+            server = new Server();
+        });
+
+        it("should only add the alamid-routes on bootstrap", function() {
+
+            server.setConfig("appDir", path.resolve(__dirname, "../integration/setup/testApp/"));
+
+            server.bootstrap();
+
+            var router = server.getRouter();
+
+            //18 alamid-default routes added on bootstrap
+            //for services and validators
+            expect(router.items.length).to.eql(18);
+        });
+    });
+    */
 
 });
