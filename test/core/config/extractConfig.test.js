95671fc10d0488c8a91f16d0d3931abd59d51b13
miku <michael.jaser@peerigon.com>
2013-02-06 15:23:08 +0100

    - refactored extract config - added second level manual deep-copy (for config.use vars)

--- test/core/config/extractConfig.test.js
+++ test/core/config/extractConfig.test.js
@@ -42,4 +42,34 @@ describe("extractConfig", function () {
         expect(resultingConfig.env).to.be(config.server.env);
         expect(resultingConfig).to.only.have.keys(["port", "useBundling", "env"]);
     });
+
+    it("should overwrite and merge the values from shared to local", function () {
+
+
+        config = {
+            "use" : {
+                "websockets" : true
+            },
+            "client" : {
+                use : {
+                    "client" : true
+                }
+            },
+            "server" : {
+            }
+        };
+
+
+        var clientConf = extractConfig(config, "client");
+        var serverConf = extractConfig(config, "server");
+
+        console.log(clientConf, serverConf);
+
+        expect(clientConf.use.websockets).to.be(true);
+        expect(clientConf.use.client).to.be(true);
+        expect(serverConf.use.websockets).to.be(true);
+        expect(serverConf.use.client).to.be(undefined);
+
+    });
+
 });
