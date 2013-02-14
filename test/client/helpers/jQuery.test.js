8fab3d07e42a5267f962bfb4feb46c00d01ff5ca
Johannes Ewald <mail@johannesewald.de>
2013-02-14 18:58:08 +0100

    - jQuery update to 1.9.1

--- test/client/helpers/jQuery.test.js
+++ test/client/helpers/jQuery.test.js
@@ -5,10 +5,6 @@ var expect = require("expect.js"),
 
 describe("jQuery", function () {
 
-    it("should be jQuery JavaScript library", function () {
-        expect(AlamidjQuery.expando.search(/alamidjQuery/)).to.equal(0);
-    });
-
     it("should not be attached to the window object", function () {
         expect(AlamidjQuery).to.not.equal(window.jQuery);
     });
