bc110f5297d513a602da80f8d51ec9fa37ca32c8
jhnns <johannes.ewald@peerigon.com>
2013-04-17 16:24:59 +0200

    - introduced Disposable.class #199

---
+++ test/shared/Disposable.class.test.js
@@ -0,0 +1,64 @@
+"use strict"; // run code in ES5 strict mode
+
+var expect = require("expect.js"),
+    Disposable = require("../../lib/shared/Disposable.class.js");
+
+describe("Disposable", function () {
+    var disposable,
+        context;
+
+    function test() {
+        context = this;
+    }
+
+    beforeEach(function () {
+        disposable = new Disposable();
+    });
+    describe("#runOnDispose()", function () {
+        it("should not execute the function before dispose() has been called", function () {
+            disposable.runOnDispose(test);
+            expect(context).to.be(undefined);
+        });
+        it("should be chainable", function () {
+            expect(disposable.runOnDispose(test)).to.be(disposable);
+        });
+    });
+    describe("#addDisposable()", function () {
+        it("should be chainable", function () {
+            var disp = {
+                dispose: test
+            };
+
+            expect(disposable.addDisposable(disp)).to.be(disposable);
+        });
+    });
+    describe("#dispose() / #runOnDispose()", function () {
+        it("should execute the function with the disposable as context", function () {
+            disposable.runOnDispose(test);
+            disposable.dispose();
+            expect(context).to.be(disposable);
+        });
+    });
+    describe("#dispose() / #addDisposable()", function () {
+        it("should dispose all registered disposables", function () {
+            var calledOn = [],
+                disp1 = {},
+                disp2 = {},
+                disp3 = {};
+
+            function disposeMock() {
+                calledOn.push(this);
+            }
+
+            disp1.dispose = disp2.dispose = disp3.dispose = disposeMock;
+
+            disposable.addDisposable(disp1);
+            disposable.addDisposable(disp2);
+            disposable.addDisposable(disp3);
+
+            disposable.dispose();
+
+            expect(calledOn).to.eql([disp1, disp2, disp3]);
+        });
+    });
+});
