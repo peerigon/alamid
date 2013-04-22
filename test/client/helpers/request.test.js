8fdeb7a2885ab4528c5e6d105ba4fd765c783076
jhnns <johannes.ewald@peerigon.com>
2013-04-22 16:52:45 +0200

    - fixed RemoteService's resolving of ressource urls - JSON parsing errors are now passed to the callback() - fixed modelStatics annotation - small fixes

--- test/client/helpers/request.test.js
+++ test/client/helpers/request.test.js
@@ -5,72 +5,60 @@ var expect = require("expect.js"),
     rewire = require("rewire");
 
 describe("request", function () {
-    describe("request", function() {
+    var request;
 
-        var request;
+    beforeEach(function(){
+        request = rewire("../../../lib/client/helpers/request.js");
+    });
 
-        beforeEach(function(){
-            request = rewire("../../../lib/client/helpers/request.js");
+    describe("HTTP-Transport", function() {
+        it("should pass all data to the http-transport", function(done) {
+
+            var httpRequestMock = function(method, url, modelData, callback) {
+                    expect(method).to.be("post");
+                    expect(url).to.be("services/blog");
+                    expect(modelData).to.eql({ da : "ta" });
+                    expect(callback).to.be.a("function");
+                    callback();
+                };
+
+            request.__set__("httpRequest", httpRequestMock);
+            request("create", "services/blog", { da : "ta" }, function (response) {
+                done();
+            });
         });
+    });
 
-        describe("HTTP-Transport", function() {
-            it("should pass all data to the http-transport", function(done) {
+    describe("Websocket-Transport", function () {
+        it("should pass all data to the websocket-transport", function(done) {
 
-                var httpRequestMock = function(method, url, modelData, callback) {
-                        expect(method).to.be("post");
-                        expect(url).to.be("services/blog");
-                        expect(modelData).to.eql({ da : "ta" });
+            var configMock = {
+                    use : {
+                        websockets : true
+                    }
+                },
+                socketMock ={
+                    emit : function(method, url, model, callback) {
+                        expect(method).to.be("create");
+                        expect(url).to.contain("services/blog");
+                        expect(model).to.eql({ da : "ta" });
                         expect(callback).to.be.a("function");
                         callback();
                     },
-                    clientMock = {
-                        instance : {}
-                    };
+                    socket : {
+                        connected : true
+                    }
+                },
+                httpRequestMock = function(method, url, modelData, callback) {
+                    done(new Error("HTTP should not be called"));
+                };
 
-                request.__set__("httpRequest", httpRequestMock);
-                request.__set__("Client", clientMock);
-                request("create", "services/blog", { da : "ta" }, function (response) {
-                    done();
-                });
-            });
-        });
+            request.__set__("httpRequest", httpRequestMock);
+            request.__set__("config", configMock);
+            request.socket = socketMock;
 
-        describe("Websocket-Transport", function () {
-            it("should pass all data to the websocket-transport", function(done) {
-
-                var configMock = {
-                        use : {
-                            websockets : true
-                        }
-                    },
-                    socketMock ={
-                        emit : function(method, url, model, callback) {
-                            expect(method).to.be("create");
-                            expect(url).to.contain("services/blog");
-                            expect(model).to.eql({ da : "ta" });
-                            expect(callback).to.be.a("function");
-                            callback();
-                        },
-                        socket : {
-                            connected : true
-                        }
-                    },
-                    clientMock = {
-                        instance: {
-                            socket: socketMock
-                        }
-                    },
-                    httpRequestMock = function(method, url, modelData, callback) {
-                        done(new Error("HTTP should not be called"));
-                    };
-
-                request.__set__("httpRequest", httpRequestMock);
-                request.__set__("config", configMock);
-                request.__set__("Client", clientMock);
-
-                request("create", "services/blog", { da : "ta" }, function (response) {
-                    done();
-                });
+            request("create", "services/blog", { da : "ta" }, function (response) {
+                done();
             });
         });
     });
