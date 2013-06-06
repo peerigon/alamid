d4e9ca460ee4bdb7dd7b3b68b1f4627ce461782b
miku <michael.jaser@peerigon.com>
2013-06-06 14:34:17 +0200

    - introduced request-debouncing for READ/GET requests - websocket adapter is striping the query-string now to accept same url-signature as http

--- test/client/helpers/request.test.js
+++ test/client/helpers/request.test.js
@@ -15,12 +15,12 @@ describe("request", function () {
         it("should pass all data to the http-transport", function(done) {
 
             var httpRequestMock = function(method, url, modelData, callback) {
-                    expect(method).to.be("post");
-                    expect(url).to.be("services/blog");
-                    expect(modelData).to.eql({ da : "ta" });
-                    expect(callback).to.be.a("function");
-                    callback();
-                };
+                expect(method).to.be("post");
+                expect(url).to.be("services/blog");
+                expect(modelData).to.eql({ da : "ta" });
+                expect(callback).to.be.a("function");
+                callback();
+            };
 
             request.__set__("httpRequest", httpRequestMock);
             request("create", "services/blog", { da : "ta" }, function (response) {
@@ -59,6 +59,143 @@ describe("request", function () {
 
             request("create", "services/blog", { da : "ta" }, function (response) {
                 done();
+            });
+        });
+    });
+
+    describe("read-request debounce", function() {
+
+        describe("http", function() {
+
+            it("should only send one single request if multiple identical requests are triggered while the first request is active", function(done) {
+
+                var emitCallCount = 0,
+                    firstCallbackResponse = null;
+
+                var configMock = {
+                    use : {
+                        websockets : false
+                    }
+                };
+
+                var httpRequestMock = function(method, url, modelData, callback) {
+
+                    emitCallCount++;
+
+                    expect(method).to.be("get");
+                    expect(url).to.contain("services/blog");
+                    expect(modelData).to.eql({ da : "ta" });
+                    expect(callback).to.be.a("function");
+
+                    setTimeout(callback, 10);
+                };
+
+                request.__set__("httpRequest", httpRequestMock);
+                request.__set__("config", configMock);
+
+                request("read", "services/blog", { da : "ta" }, function (response) {
+                    console.log("first responded");
+                    firstCallbackResponse = response;
+                });
+
+                request("read", "services/blog", { da : "ta" }, function (response) {
+                    console.log("second responded");
+                    expect(emitCallCount).to.eql(1);
+                    expect(response).to.eql(firstCallbackResponse);
+                    done();
+                });
+            });
+        });
+
+        describe("websockets", function() {
+
+            it("should only send a single request if multiple identical requests are triggered while the first request is active", function(done) {
+
+                var emitCallCount = 0,
+                    firstCallbackResponse = null;
+
+                var configMock = {
+                        use : {
+                            websockets : true
+                        }
+                    },
+                    socketMock ={
+                        emit : function(method, url, model, callback) {
+
+                            emitCallCount++;
+
+                            expect(method).to.be("read");
+                            expect(url).to.contain("services/blog");
+                            expect(model).to.eql({ da : "ta" });
+                            expect(callback).to.be.a("function");
+
+                            setTimeout(callback, 10);
+                        },
+                        socket : {
+                            connected : true
+                        }
+                    },
+                    httpRequestMock = function() {
+                        done(new Error("HTTP should not be called"));
+                    };
+
+                request.__set__("httpRequest", httpRequestMock);
+                request.__set__("config", configMock);
+                request.socket = socketMock;
+
+                request("read", "services/blog", { da : "ta" }, function (response) {
+                    firstCallbackResponse = response;
+                });
+
+                request("read", "services/blog", { da : "ta" }, function (response) {
+                    expect(emitCallCount).to.eql(1);
+                    expect(response).to.eql(firstCallbackResponse);
+                    done();
+                });
+            });
+
+            it("should not debounce on other method than read", function(done) {
+
+                var emitCallCount = 0,
+                    firstCallbackResponse = false;
+
+                var configMock = {
+                        use : {
+                            websockets : true
+                        }
+                    },
+                    socketMock ={
+                        emit : function(method, url, model, callback) {
+
+                            emitCallCount++;
+
+                            expect(method).to.be("create");
+                            expect(url).to.contain("services/blog");
+                            expect(model).to.eql({ da : "ta" });
+                            expect(callback).to.be.a("function");
+
+                            setTimeout(callback, 10);
+                        },
+                        socket : {
+                            connected : true
+                        }
+                    },
+                    httpRequestMock = function() {
+                        done(new Error("HTTP should not be called"));
+                    };
+
+                request.__set__("httpRequest", httpRequestMock);
+                request.__set__("config", configMock);
+                request.socket = socketMock;
+
+                request("create", "services/blog", { da : "ta" }, function (response) {
+                    firstCallbackResponse = response;
+                });
+
+                request("create", "services/blog", { da : "ta" }, function (response) {
+                    expect(emitCallCount).to.eql(2);
+                    done();
+                });
             });
         });
     });
