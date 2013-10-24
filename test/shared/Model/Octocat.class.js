04fa0684c15062e6bc1f98167731212d9d04d111
Johannes Ewald <johannes.ewald@peerigon.com>
2013-10-24 15:45:24 +0200

    - renamed Model.prototype._schema to Model.prototype._localSchema - setSchema without schemaType now sets the sharedSchema by default - a local schema always overrides a shared schema

--- test/shared/Model/Octocat.class.js
+++ test/shared/Model/Octocat.class.js
@@ -1,15 +1,15 @@
 "use strict";
 
 var Model = require('../../../lib/shared/Model.class.js'),
-    schema = require("./schemas/OctocatSchema.js"),
+    sharedSchema = require("./schemas/OctocatSchema.js"),
     serverSchema = require("./schemas/OctocatSchema.server.js");
 
 var Octocat = Model.extend("Octocat", {
     url: "Octocat",
     constructor: function(id) {
         this._super(id);
-        this.setSchema(schema, "shared");
-        this.setSchema(serverSchema);
+        this.setSchema(sharedSchema, "shared");
+        this.setSchema(serverSchema, "local");
     }
 });
 
